bd036b5e0294507d44d0142bab75c9ae3bf50f8c
vivekdogra <vivek.dogra.iitd@gmail.com>
2014-10-01 13:14:49 +0530

    index.js modified to create https server. key and cert files added.

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 'use strict';
 
+var fs = require('fs');
+var https = require('https');
+var express = require('express');
+var kraken = require('kraken-js');
 
-var kraken = require('kraken-js'),
-    app = require('express')(),
-    options = {
-        onconfig: function (config, next) {
-            //any config setup/overrides here
-            next(null, config);
-        }
-    },
-    port = process.env.PORT || 8000;
+var options, app, server, port;
 
 
+options = {
+    key: fs.readFileSync('./privkey.key'),
+    cert: fs.readFileSync('./cacert.cert'),
+    onconfig: function (config, next) {
+
+        //any config setup/overrides here
+        next(null, config);
+    }
+};
+
+port = process.env.PORT || 8002;
+
+app = express();
 app.use(kraken(options));
 
-
-app.listen(port, function (err) {
-    console.log('[%s] Listening on http://localhost:%d', app.settings.env, port);
+server = https.createServer(options, app);
+server.listen(port, function () {
+    console.log('[%s] Listening on https://localhost:%d', app.settings.env, port);
 });
