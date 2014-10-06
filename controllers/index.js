963ff433d37998a501ea2120370713a29236c62c
vivekdogra <vivek.dogra.iitd@gmail.com>
2014-10-06 12:09:57 +0530

    Login page created. index controller changed to serve get and post request for login.

--- controllers/index.js
+++ controllers/index.js
@@ -8,11 +8,13 @@ module.exports = function (router) {
 
     var model = new IndexModel();
 
-
-    router.get('/', function (req, res) {
-        
-        res.render('index', model);
-        
+    // for get request from web browser
+    router.get('/login', function(req, res) {
+        res.render('login', model);
     });
 
+    // for post request from facebook
+    router.post('/login', function(req, res) {
+        res.render('login', model);
+    });
 };
