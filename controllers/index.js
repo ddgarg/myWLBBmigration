6085d1d0cd95593acaff90838908087a9aaddd32
vivekdogra <vivek.dogra.iitd@gmail.com>
2014-10-06 15:31:03 +0530

    Logout functionality added.

--- controllers/index.js
+++ controllers/index.js
@@ -8,13 +8,37 @@ module.exports = function (router) {
 
     var model = new IndexModel();
 
+    router.get('/', function(req, res) {
+        res.redirect('/login');
+    });
+
     // for get request from web browser
     router.get('/login', function(req, res) {
-        res.render('login', model);
+        if (req.session.fbAccessToken) {
+            res.redirect('/mywishlist');
+        }
+        else {
+            // render login page if fbAccessToken is not available in session
+            res.render('login', model);
+        }
+
     });
 
     // for post request from facebook
     router.post('/login', function(req, res) {
         res.render('login', model);
+    });
+
+    router.get('/logout', function(req, res) {
+        // destroy the session and redirect the user to login page.
+        req.session.destroy(function(err){
+            if(err){
+                console.log(err);
+            }
+            else
+            {
+                res.redirect('/login');
+            }
+        });
     });
 };
