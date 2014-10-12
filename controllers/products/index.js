8fdb3129345daecfcdba9f63248c079ac56ac9a7
vivekdogra <vivek.dogra.iitd@gmail.com>
2014-10-12 11:07:29 +0530

    Auto suggestion in search box, misspelled name handle through google and minor styling here and there.

--- controllers/products/index.js
+++ controllers/products/index.js
@@ -4,6 +4,10 @@
 var IndexModel  = require('../../models/index');
 var amazon      = require('apac').OperationHelper;
 var util        = require('util');
+var request       = require('request');
+var cheerio       = require('cheerio');
+
+var amazonProductNotMatchedError = 'AWS.ECommerceService.NoExactMatches';
 
 module.exports = function (router) {
 
@@ -14,26 +18,90 @@ module.exports = function (router) {
         res.send({'response': 'OK'});
     });
 
+    function amazonCaller(amazonHelper, searchQuery, res) {
+        amazonHelper.execute('ItemSearch', {
+                'SearchIndex': 'All',
+                'Keywords': searchQuery,
+                'ResponseGroup': 'ItemAttributes,Offers,Images'
+            }, function (results, onAmazonError) {
+
+                amazonResponseHasError(results, searchQuery, googleSuggestedQuery, function (queryStatus) {
+                    console.log(queryStatus);
+                    if (!queryStatus.callAmazonAgain && queryStatus.suggestedQuery)
+                    {
+                        formatAmazonResponse(results, function (formattedResponse) {
+                            console.log("sendingfromcallback");
+                            res.send(formattedResponse);
+                        });
+                    }
+                    else if(queryStatus.suggestedQuery){
+                        amazonCaller(amazonHelper, queryStatus.suggestedQuery, res);
+                    }
+                    else {
+                        errorFromAmazon(res, {'error': 'error'});
+                    }
+
+                });
+
+
+            }
+        );
+    }
+
+    var amazonResponseHasError = function(results, searchQuery, googleSuggestedQuery, callback){
+        var amazonRequest = results.ItemSearchResponse.Items[0].Request[0];
+
+
+        if (amazonRequest.hasOwnProperty("Errors"))
+        {
+            var amazonError = amazonRequest.Errors[0].Error[0].Code[0];
+            if(amazonError === amazonProductNotMatchedError)
+            {
+             googleSuggestedQuery(searchQuery, callback);
+            }
+        }
+        else {
+            callback({'suggestedQuery': searchQuery, 'callAmazonAgain': false});
+        }
+
+    };
+
+
+    var googleSuggestedQuery = function(searchQuery, callback){
+
+        request('https://www.google.co.in/search?q='+ searchQuery, function (error, response, html) {
+            if (!error && response.statusCode == 200) {
+                var $ = cheerio.load(html);
+                var message = $("a.spell").attr('href');
+
+                if(typeof message ==  'undefined')
+                {
+                    callback({'suggestedQuery': null, 'callAmazonAgain': false});
+                }
+                else {
+                    var suggestedSearchQuery = message.match(/q=(.*?)\&/)[1].replace(/\+/g," ");
+                    callback({'suggestedQuery' : suggestedSearchQuery, 'callAmazonAgain': true});
+                }
+            }
+            else
+            {
+                return {'suggestedQuery': null, 'callAmazonAgain': false};
+            }
+
+        });
+    };
+
     router.post('/searchproducts', function(req, res) {
         var searchQuery = req.body.name;
         var amazonConfig = req.app.kraken.get('amazon-config');
         var amazonHelper = new amazon (amazonConfig);
         console.log('searchQuery' + searchQuery);
-        amazonHelper.execute('ItemSearch', {
-                'SearchIndex': 'All',
-                'Keywords': searchQuery,
-                'ResponseGroup': 'ItemAttributes,Offers,Images'
-            }, function(results, onAmazonError) { // you can add a third parameter for the raw xml response, 'results' here are currently parsed using xml2js
-                    formatAmazonResponse(results, function(formattedResponse) {
-                        res.send(formattedResponse);
-                    });
-            }
-        );
-
+        amazonCaller(amazonHelper, searchQuery, res);
     });
 
-    var onAmazonError = function (error){
-        console.log('error in Amazon call');
+    var errorFromAmazon = function (res, error){
+
+        res.send({"error" :error});
     };
 
     var formatAmazonResponse = function(amazonRes, callback){
@@ -47,7 +115,7 @@ module.exports = function (router) {
             finalItem.asin     =    amazonItems.Item[item].ASIN[0];
             finalItem.item_link =   amazonItems.Item[item].DetailPageURL[0];
             finalItem.item_image=   amazonItems.Item[item].ImageSets[0].ImageSet[0].MediumImage[0].URL[0];
-            if (amazonItems.Item[item].OfferSummary)
+            if (amazonItems.Item[item].OfferSummary && amazonItems.Item[item].OfferSummary[0].LowestNewPrice)
             {
                 finalItem.price =       amazonItems.Item[item].OfferSummary[0].LowestNewPrice[0].FormattedPrice[0];
             }
@@ -55,7 +123,7 @@ module.exports = function (router) {
             formattedResponse.push(finalItem);
         }
         var productArray = {'products': formattedResponse};
-        console.log(productArray);
+//        console.log(productArray);
         callback(productArray);
     };
 };
