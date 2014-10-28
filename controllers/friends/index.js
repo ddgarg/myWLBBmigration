24c2b85265b4446d7c0a7989f1fea41fee9124af
vivekdogra <vivek.dogra.iitd@gmail.com>
2014-10-28 18:24:55 +0530

    Friends controller.

---
+++ controllers/friends/index.js
@@ -0,0 +1,48 @@
+'use strict';
+
+var fbHelper = require('../../lib/fbHelper');
+var wishlist      = require('../../models/wishlistModel');
+
+
+module.exports = function (router) {
+
+    var wishlistModel= wishlist.wishlistModel();
+
+    router.get('/', function(req, res){
+
+        if(req.session.fbAccessToken) {
+            fbHelper.getFriends(req, function(fbResponse){
+               // res.send(fbResponse);
+                res.render('friends', fbResponse);
+            });
+        }
+        else
+        {
+            res.redirect('/login');
+        }
+
+    });
+
+    router.get('/:userId', function(req, res) {
+
+        if (req.session.fbAccessToken)
+        {
+            wishlistModel.find({'userId' : req.params.userId}, function (err, wishes) {
+                if (err) {
+                    logger.error('mongo err : ' + err);
+                    res.redirect('/mywishlist');
+                }
+                else{
+                    var myWishlist = { wishList: wishes };
+                    res.render('mywishlist', myWishlist);
+                }
+            });
+        }
+        else {
+            // redirect the user to login page if fbAccessToken is not available in session
+            res.redirect('/');
+        }
+
+    });
+
+};
